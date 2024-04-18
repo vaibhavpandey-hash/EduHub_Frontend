235213700802bfdaa1b7bef4f03c97b260c13d61
Vaibhav <vaibhavrajpandey@yahoo.com>
2024-04-18 20:13:09 +0530

    Added password matching validation

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css"
+import Header from "./components/common/header/Header"
+
+import Contact from "./components/contact/Contact"
+import Footer from "./components/common/footer/Footer"
+import Home from "./components/home/Home"
+function App() {
+  return (
+    <>
+
+        <Header />
+        <Home/>
+        <Contact />
+        <Footer />
+
+    </>
+  )
+}
+
+export default App
